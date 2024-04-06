62d29c8d78d40ff90323fac9059dbe7a43c57494
ZloyXleb <164645834+ZloyXleb@users.noreply.github.com>
2024-04-06 17:12:35 +0300

    Add files via upload
    добавлены ракетки и управление

--- pinpong.py
+++ pinpong.py
@@ -26,12 +26,18 @@ class GameSprite(sprite.Sprite):
         my_win.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def updateL(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 620:
-            self.rect.x += self.speed
+        if keys_pressed[K_p] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_l] and self.rect.y < 450:
+            self.rect.y += self.speed
+    def updateR(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 450:
+            self.rect.y += self.speed
     
 
 
@@ -39,8 +45,8 @@ class Player(GameSprite):
 
             
 
-
-
+l_p = Player('raketka.png', 0, 200, 50, 150, 4)
+r_p = Player('raketka.png', 650, 200, 50, 150, 4)
 
 
 
@@ -73,6 +79,10 @@ while game:
 
 
     my_win.blit(fon, (0, 0))
-        
+    l_p.updateL()
+    r_p.updateR()
+    l_p.reset()
+    r_p.reset()
+    
     clock.tick(60)
     display.update()
